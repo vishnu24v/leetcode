e3d2df0c53dececa0537a15c16da738d14c3aff3
vishnu vaishnav <162352686+vishnu24v@users.noreply.github.com>
2024-04-05 06:51:08 +0530

    Time: 4 ms (42.41%), Space: 7.7 MB (64.31%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        vector<char>st;
+
+        st.push_back(s[0]);
+
+        for(int i=1;i<s.size();i++){
+
+            // value at last index. or at the top of stack.
+            char topval = st.back();
+
+
+            // if pair occur i.e one small and one capital
+                if( !st.empty() && abs(topval - s[i]) == 32){
+                    st.pop_back();
+                }else{
+                    st.push_back(s[i]);
+                }
+
+        }
+
+        // store in string ans.
+     string ans(st.begin(), st.end());
+
+        return ans;
+    }
+};
