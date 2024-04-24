04936c590af3a28f87e99ee1c63d0cc9fef2f7ce
vishnu vaishnav <162352686+vishnu24v@users.noreply.github.com>
2024-04-24 06:53:45 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (23.43%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n<=2){
+            return 1;
+        } else {
+            // n is greater equal to 3
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            int index =3;
+            int sum = 0;
+
+            while(index<=n){
+                sum  = a+b+c;
+                index++;
+                a=b;
+                b = c;
+                c =sum;
+            }
+            return sum;
+    }
+}
+}
