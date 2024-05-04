c397ff9816270b17b448f2da2c7a29443a98e077
vishnu vaishnav <162352686+vishnu24v@users.noreply.github.com>
2024-05-04 07:14:58 +0530

    Time: 16 ms (97.17%), Space: 54.9 MB (22.99%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+    int boats = 0;
+
+    // Use 2 pointers to find the heaviest and lightest person
+    int left = 0, right = people.length - 1;
+
+    while (left <= right) {
+
+      // If heaviest and lightest person can fit in same boat
+      if (people[left] + people[right] <= limit) {
+        left++;
+      }
+
+      // In any case, the heaviest person will be on the boat
+      right--;
+
+      // Increment the number of boats
+      boats++;
+    }
+
+    return boats;
+    }
+}
